366c09544d1f9177699e07ce468acbd4d13bc8c6
Toshiaki Maki <tmaki@pivotal.io>
2019-06-13 00:58:42 +0900

    up

--- deploy-jumpbox.sh
+++ deploy-jumpbox.sh
@@ -4,9 +4,6 @@ bosh -d jumpbox deploy jumpbox.yml \
   -l versions.yml \
   -o ops-files/jq.yml \
   -o ops-files/openjdk.yml \
-  -o ops-files/mysql-client.yml \
-  -o ops-files/psql.yml \
-  -o ops-files/awscli.yml \
   -o ops-files/cf-cli.yml \
   -o ops-files/git.yml \
   -o ops-files/persistence-home.yml \
